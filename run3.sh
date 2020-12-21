bb0e20b568ec414f03b1e8ce6cc299638cc19c30
Joel Joseph <joeljosephjin@gmail.com>
2020-12-21 22:46:00 +0530

    run3

--- run3.sh
+++ run3.sh
@@ -3,5 +3,5 @@ MANY="--n_layers 2 --n_hiddens 100 --data_path data/ --log_every 100 --samples_p
 SEED=0
 
 #3. lamaml MANY MNIST DATASETS
-python3 main.py $MANY --model lamaml --memories 500 --batch_size 10 --replay_batch_size 10 --n_epochs 1 --glances 10 --opt_lr 0.1 \
+python3 mainwb.py $MANY --model lamaml --memories 500 --batch_size 10 --replay_batch_size 10 --n_epochs 1 --glances 10 --opt_lr 0.1 \
                     --alpha_init 0.1 --learn_lr --use_old_task_memory --seed $SEED

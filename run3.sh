440020f806aac8264fae5762ee4e497b5c446140
Joel Joseph <joeljosephjin@gmail.com>
2020-12-21 22:45:35 +0530

    run3

---
+++ run3.sh
@@ -0,0 +1,7 @@
+MANY="--n_layers 2 --n_hiddens 100 --data_path data/ --log_every 100 --samples_per_task 200 --dataset mnist_manypermutations --cuda --log_dir logs/"
+
+SEED=0
+
+#3. lamaml MANY MNIST DATASETS
+python3 main.py $MANY --model lamaml --memories 500 --batch_size 10 --replay_batch_size 10 --n_epochs 1 --glances 10 --opt_lr 0.1 \
+                    --alpha_init 0.1 --learn_lr --use_old_task_memory --seed $SEED

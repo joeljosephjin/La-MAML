ecf205f2106e19fc52683a4c208834bb93d527aa
Joel Joseph <joeljosephjin@gmail.com>
2020-12-27 11:39:25 +0530

    seed

--- run_rots.sh
+++ run_rots.sh
@@ -1,4 +1,5 @@
 ROT="--n_layers 2 --n_hiddens 100 --data_path data/ --log_every 100 --samples_per_task 1000 --dataset mnist_rotations    --cuda --log_dir logs/"
+SEED=0
 
 #cmaml ROTATION MNIST DATASETS
 python3 mainwb.py $ROT --model lamaml --memories 200 --batch_size 10 --replay_batch_size 10 --n_epochs 1 --glances 5 --opt_lr 0.1 \

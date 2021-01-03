7051a40d00ad4998fd777d0829d137df8706c1f7
Joel Joseph <joeljosephjin@gmail.com>
2021-01-03 09:25:44 +0530

    more

--- codeocean_run.sh
+++ codeocean_run.sh
@@ -4,15 +4,21 @@ apt-get install wget
 
 apt-get install python3-pip # terminal
 
-pip install ipdb
+pip3 install ipdb
 
-conda install matplotlib numpy pillow torchvision urllib3 scipy # terminal
+conda install matplotlib numpy pillow torchvision urllib3 scipy --yes # terminal
 
-conda update -n base -c defaults conda #terminal
+conda update -n base -c defaults conda --yes #terminal
 
-conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch # terminal
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch --yes # terminal
 
 cd La-MAML
+
+git pull https://github.com/joeljosephjin/La-MAML
+
+pip3 install wandb
+
+wandb login 665a5d573c302c27f7dab355484da17a460e6759
 
 chmod +x run_many.sh
 ./run_many.sh

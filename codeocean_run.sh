8fdb62dbaa2aa5d5647143d33e3bd531cd710d0d
Joel Joseph <joeljosephjin@gmail.com>
2021-01-03 09:17:59 +0530

    codeocean runner

---
+++ codeocean_run.sh
@@ -0,0 +1,18 @@
+apt-get update
+
+apt-get install wget
+
+apt-get install python3-pip # terminal
+
+pip install ipdb
+
+conda install matplotlib numpy pillow torchvision urllib3 scipy # terminal
+
+conda update -n base -c defaults conda #terminal
+
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch # terminal
+
+cd La-MAML
+
+chmod +x run_many.sh
+./run_many.sh

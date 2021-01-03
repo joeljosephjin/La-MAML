1d6abad8e86a9a4588f3d416e7ab3d26f7597285
Joel Joseph <joeljosephjin@gmail.com>
2021-01-03 10:33:31 +0530

    more

--- codeocean_run.sh
+++ codeocean_run.sh
@@ -22,8 +22,6 @@ conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch --yes #
 
 # git pull https://github.com/joeljosephjin/La-MAML
 
-conda install -c conda-forge wandb --yes
-
 wandb login 665a5d573c302c27f7dab355484da17a460e6759
 
 chmod +x run_many.sh

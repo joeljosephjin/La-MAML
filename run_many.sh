4edec6078ea0d535cb2eba38c703dcbdc6058b1a
Joel Joseph <joeljosephjin@gmail.com>
2021-01-03 11:11:33 +0530

    next

--- run_many.sh
+++ run_many.sh
@@ -6,10 +6,10 @@ SEED=0
 # python3 mainwb.py $MANY --model lamaml --memories 500 --batch_size 10 --replay_batch_size 10 --n_epochs 1 --glances 5 --opt_wt 0.15 \
 #                     --alpha_init 0.03 --sync_update --use_old_task_memory --seed $SEED
 
-# #sync MANY MNIST DATASETS
-# python3 mainwb.py $MANY --model lamaml --memories 500 --batch_size 10 --replay_batch_size 10 --n_epochs 1 --glances 10 --opt_lr 0.1 \
-#                     --opt_wt 0.03  --alpha_init 0.03 --learn_lr --sync_update --use_old_task_memory --seed $SEED
-
-#lamaml MANY MNIST DATASETS
+#sync MANY MNIST DATASETS
 python3 mainwb.py $MANY --model lamaml --memories 500 --batch_size 10 --replay_batch_size 10 --n_epochs 1 --glances 10 --opt_lr 0.1 \
-                    --alpha_init 0.1 --learn_lr --use_old_task_memory --seed $SEED
+                    --opt_wt 0.03  --alpha_init 0.03 --learn_lr --sync_update --use_old_task_memory --seed $SEED
+
+# #lamaml MANY MNIST DATASETS
+# python3 mainwb.py $MANY --model lamaml --memories 500 --batch_size 10 --replay_batch_size 10 --n_epochs 1 --glances 10 --opt_lr 0.1 \
+#                     --alpha_init 0.1 --learn_lr --use_old_task_memory --seed $SEED

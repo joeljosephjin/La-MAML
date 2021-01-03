fa5a6ae251a76d2135f4483c897a24bd0b8b4927
Joel Joseph <joeljosephjin@gmail.com>
2021-01-03 15:36:27 +0530

    run2

--- codeocean_run.sh
+++ codeocean_run.sh
@@ -30,8 +30,8 @@ wandb login 665a5d573c302c27f7dab355484da17a460e6759
 CIFAR='--data_path data/ --log_every 100 --dataset cifar100 --cuda --log_dir logs/'
 SEED=0
 
-##### ER ##### CIFAR DATASET Multi-Pass 
-python3 mainwb.py $CIFAR --model eralg4 --expt_name eralg4 --memories 200 --batch_size 10 --replay_batch_size 1 --n_epochs 10 \
-                     --lr 0.03 --glances 1 --loader class_incremental_loader  --increment 5 \
-                    --arch "pc_cnn" --log_every 3125 --class_order random \
-                    --seed $SEED --calc_test_accuracy --validation 0.1
+##### La-MAML ##### CIFAR DATASET Multi-Pass
+python3 mainwb.py $CIFAR --model lamaml_cifar --expt_name lamaml --memories 200 --batch_size 10 --replay_batch_size 10 --n_epochs 10 \
+                    --opt_lr 0.25 --alpha_init 0.1 --opt_wt 0.1 --glances 1 --loader class_incremental_loader --increment 5 \
+                    --arch "pc_cnn" --cifar_batches 5 --learn_lr --log_every 3125 --second_order --class_order random \
+                    --seed $SEED --grad_clip_norm 1.0 --calc_test_accuracy --validation 0.1

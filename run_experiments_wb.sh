1fee0bea02bf240c163785a8589ac1bff5ed7016
Joel Joseph <joeljosephjin@gmail.com>
2021-01-26 17:31:19 +0530

    bti wandb

--- run_experiments_wb.sh
+++ run_experiments_wb.sh
@@ -13,14 +13,17 @@ SEED=0
 #cmaml ROTATION MNIST DATASETS
 python3 mainwb.py $ROT --model lamaml --memories 200 --batch_size 10 --replay_batch_size 10 --n_epochs 1 --glances 5 --opt_lr 0.1 \
                     --opt_wt 0.1 --alpha_init 0.1 --sync_update --use_old_task_memory --seed $SEED
+# tip: cmaml has --sync_update without --learn_lr
 
 #sync ROTATION MNIST DATASETS
 python3 mainwb.py $ROT --model lamaml --memories 200 --batch_size 10 --replay_batch_size 10 --n_epochs 1 --glances 5 --opt_lr 0.1 \
                     --opt_wt 0.3 --alpha_init 0.15 --learn_lr --sync_update --use_old_task_memory --seed $SEED
+# tip: sync has both --sync_update and --learn_lr
 
 #lamaml ROTATION MNIST DATASETS
 python3 mainwb.py $ROT --model lamaml --memories 200 --batch_size 10 --replay_batch_size 10 --n_epochs 1 --glances 5 --opt_lr 0.3 \
                     --alpha_init 0.15 --learn_lr --use_old_task_memory --seed $SEED
+# tip: lamaml has --learn_lr without --sync_update
 
 #PERMUTATION
 #cmaml PERMUTATION MNIST DATASETS
